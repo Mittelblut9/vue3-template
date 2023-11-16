e1db9bd91b05061ef10b1fedb4397c9dad20b2a2
Mittelblut9 <info@blackdayz.de>
2023-11-16 20:06:01 +0100

    fix: update lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    '*.{js,jsx,vue,md,mjs,yml}': 'vue-cli-service lint',
-    '*.{css,scss,vue}': 'stylelint',
+    '*.{js,vue}': 'npm run eslint:lint',
+    '*.{css,scss,vue}': 'npm run stylelint:lint',
 };
